97461bae95eb98cac79fdce7f8a5e7050e61fae8
jominjun <mj3029327@naver.com>
2021-01-01 16:51:50 +0900

    Learned Fancy Softmax Classifier
    Learned Fancy Softmax Classifier

--- 04_SoftmaxRegression/02_FancySoftmaxClassifier.py
+++ 04_SoftmaxRegression/02_FancySoftmaxClassifier.py
@@ -18,4 +18,70 @@
 import numpy as np
 import tensorflow as tf
 
-# 
+# Load Dataset
+xy = np.loadtxt("C:\Programming\Python\AI\learnML_basic\data-04-zoo.csv", delimiter=",", dtype=np.float32)
+
+x_data = xy[:, 0:-1]
+y_data = xy[:, [-1]]
+
+# 오류 해결을 위해
+# Error Msg | InvalidArgumentError: Value for attr 'TI' of float is not in the list of allowed values: uint8, int32, int64; NodeDef: {{node OneHot}}; Op<name=OneHot; signature=indices:TI, depth:int32, on_value:T, off_value:T -> output:T; attr=axis:int,default=-1; attr=T:type; attr=TI:type,default=DT_INT64,allowed=[DT_UINT8, DT_INT32, DT_INT64]> [Op:OneHot]
+y_data = tf.dtypes.cast(y_data, tf.int32)
+
+nb_classes = 7
+
+# Make Y data as onehot shape
+Y_one_hot = tf.one_hot(list(y_data), nb_classes)
+Y_one_hot = tf.reshape(Y_one_hot, [-1, nb_classes])
+
+# Softmax Classifier
+# weight & bias setting
+W = tf.Variable(tf.random.normal([16, nb_classes]), name="weight")
+b = tf.Variable(tf.random.normal([nb_classes]), name="bias")
+
+variables = [W, b]
+
+# tf.nn.softmax computes softmax activations
+def logit_fn(X):
+  return tf.matmul(X, W) + b
+
+def hypothesis(X):
+  return tf.nn.softmax(logit_fn(X))
+
+def cost_fn(X, Y):
+  logits = logit_fn(X)
+  cost_i = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y)
+
+  cost = tf.reduce_mean(cost_i)
+  return cost
+
+def grad_fn(X, Y):
+  with tf.GradientTape() as tape:
+    loss = cost_fn(X, Y)
+    grads = tape.gradient(loss, variables)
+    
+    return grads
+
+def prediction(X, Y):
+  pred = tf.argmax(hypothesis(X), 1)
+  correct_prediction = tf.equal(pred, tf.argmax(Y, 1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+  return accuracy
+
+def fit(X, Y, epochs=1000, verbose=100):
+  optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.1)
+
+  for i in range(epochs):
+    grads = grad_fn(X, Y)
+    optimizer.apply_gradients(zip(grads, variables))
+
+    if (not i) | (not ((i+1) % verbose)):
+      acc = prediction(X, Y).numpy()
+      loss = tf.reduce_sum(cost_fn(X, Y)).numpy()
+
+      print("Loss & Acc at {} epoch {}, {}".format(i+1, loss, acc))
+
+# Run
+if __name__ == "__main__":
+  fit(x_data, Y_one_hot)
