9429040209aa19b1fc746b163a61c405b6bc2b26
jominjun <mj3029327@naver.com>
2021-01-02 19:56:13 +0900

    Learned Data Preprocessing

--- 05_Application&Tips/01_LearningRate&DataPreprocessing.py
+++ 05_Application&Tips/01_LearningRate&DataPreprocessing.py
@@ -46,4 +46,20 @@
 
 # =={ Data Preprocessing }==
 
+# Noisy Data. 즉, 쓸모없는 데이터들을 잘 처리해주는 것이 가장 중요!
+
 # 1. Feature Scaling
+# 대부분의 값이 한 쪽에 몰려있는데 일부 값들이 다른 쪽에 위치할 때, 해당 값들의 분포까지 잘 나타내게 하기 위한 방법
+# ex) 단위가 서로 다른 값에 대하여 => 174(cm) VS 88(kg)
+
+# - 01. Standardization (Mean distance) : 표준화 기법 (주로 사용)
+# : X_new = (x-μ)/σ
+# : X값을 평균으로 뺀 것을 표준편차로 나눈다 = 평균에서 얼마나 떨어져 있는가?
+
+# - 02. Normalization (0~1) : 정규화 기법
+# : X_new = (X - X_min) / (X_max - X_min)
+# : 실제 X값을 최솟값으로 뺀 것을 최댓값과 최솟값의 차로 나눈 것
+
+# [Python Code(numpy)]
+# Standardization = (data - np.mean(data)) / sqrt(np.sum(data - np.mean(data))^2) / np.count(data)
+# Normalization = (data - np.min(data, 0)) / (np.max(data, 0) - np.min(data, 0))
